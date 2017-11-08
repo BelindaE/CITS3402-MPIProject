e91e114012f7b0950983e153fdece1b3a0ece44a
calvidler <32976906+calvidler@users.noreply.github.com>
2017-11-08 20:13:56 +0800

    Update mpi7.c

--- mpi7.c
+++ mpi7.c
@@ -9,7 +9,7 @@
 
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_BLUE    "\x1b[34m" 
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
 
 //  compile:  gcc -fopenmp -o s sequential1.c
